14df7eb9a44703fe1468603b53d8124970283c6b
张卓 <zhangzhuo2018@yeah.net>
2017-06-12 15:13:05 +0800

    update weather

--- android/app/src/main/java/com/coolweather/android/util/Utility.java
+++ android/app/src/main/java/com/coolweather/android/util/Utility.java
@@ -5,17 +5,15 @@ import android.text.TextUtils;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * Created by admin on 2017/6/7.
- */
-
 public class Utility {
+
     /**
      * 解析和处理服务器返回的省级数据
      */
@@ -82,6 +80,21 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
